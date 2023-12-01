11f23bb5798b2b970608e628410b100df80adee8
debanirmalya <debanirmalyapatra@gmail.com>
2023-12-01 15:22:57 +0530

    project list fixes

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,50 +1,50 @@
 // App.js
-import React from 'react';
-import ProjectCard from './ ProjectCard';
-import Cards from './cards';
+import React from "react";
+import ProjectCard from "./ ProjectCard";
+import Cards from "./cards";
 
 const projects = [
-    {
-      title: 'Project 1',
-      description: 'Description for Project 1',
-      imageUrl: 'https://placekitten.com/300/150',
-      githubLink: 'https://github.com/your-username/project1',
-    },
-    {
-      title: 'Project 2',
-      description: 'Description for Project 2',
-      imageUrl: 'https://placekitten.com/300/150',
-      githubLink: 'https://github.com/your-username/project2',
-    },
+	{
+		title: "Project 1",
+		description: "Description for Project 1",
+		imageUrl: "https://placekitten.com/300/150",
+		githubLink: "https://github.com/your-username/project1",
+	},
+	{
+		title: "Project 2",
+		description: "Description for Project 2",
+		imageUrl: "https://placekitten.com/300/150",
+		githubLink: "https://github.com/your-username/project2",
+	},
 
-    {
-        title: 'Project 2',
-        description: 'Description for Project 2',
-        imageUrl: 'https://placekitten.com/300/150',
-        githubLink: 'https://github.com/your-username/project2',
-      },
+	{
+		title: "Project 2",
+		description: "Description for Project 2",
+		imageUrl: "https://placekitten.com/300/150",
+		githubLink: "https://github.com/your-username/project2",
+	},
 
-      {
-        title: 'Project 2',
-        description: 'Description for Project 2',
-        imageUrl: 'https://placekitten.com/300/150',
-        githubLink: 'https://github.com/your-username/project2',
-      },
-    // Add more projects as needed
-  ];
-  
-  const ProjectList = () => {
-  return (
-    <div>
-      <h1>Our Projects</h1>
-      {/* <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+	{
+		title: "Project 2",
+		description: "Description for Project 2",
+		imageUrl: "https://placekitten.com/300/150",
+		githubLink: "https://github.com/your-username/project2",
+	},
+	// Add more projects as needed
+];
+
+const ProjectList = () => {
+	return (
+		<div style={{ margin: "15vh auto" }}>
+			<h1>Our Projects</h1>
+			{/* <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         {projects.map((project, index) => (
           <ProjectCard key={index} {...project} />
         ))}
       </div> */}
-      <Cards/>
-    </div>
-  );
+			<Cards />
+		</div>
+	);
 };
 
 export default ProjectList;
