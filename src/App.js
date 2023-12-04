bdcd6f087b459ea082481b3b4a77c0665d969a6f
sarvesh raut <sarveshraut@sarveshs-MacBook-Air.local>
2023-12-04 09:43:51 +0530

    fixed routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // App.js
 
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header";
 import HighRes from "./components/high-res";
